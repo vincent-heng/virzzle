68c7517160b6c44be160bef5521694d804cf8627
megal000l <disturbman52@gmail.com>
2015-11-21 13:59:20 +0100

    prefab for Doors, KEys and Locks + script update
    prefab for Doors, KEys and Locks + script update

--- Assets/Scripts/playerBehaviour.cs
+++ Assets/Scripts/playerBehaviour.cs
@@ -1,10 +1,9 @@
-﻿using UnityEngine;
+﻿ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
 public class playerBehaviour : MonoBehaviour {
-	
-	public float force;
+
 	private List<GameObject> inventory = new List<GameObject>();
 
 	void OnTriggerEnter( Collider col)
