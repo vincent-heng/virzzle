70ead56c93d914b0ef94bf665d2eb699a237af9c
vincent-heng <hengvincent.1994@gmail.com>
2015-11-21 13:56:18 +0100

    Pick an item feature

--- Assets/Scripts/playerBehaviour.cs
+++ Assets/Scripts/playerBehaviour.cs
@@ -5,13 +5,32 @@ using System.Collections.Generic;
 public class playerBehaviour : MonoBehaviour {
 	
 	public float force;
-	private List<GameObject> inventory = new List<GameObject>();
+	public float armLength = 20;
 
-	void OnTriggerEnter( Collider col)
-	{		
-		if (col.gameObject.tag == "key") {
-			inventory.Add(col.gameObject);
-			col.gameObject.SetActive(false);
+	private GameObject handedObject = new GameObject();
+
+	private bool takeItem() {
+		RaycastHit hit;
+		Vector3 playerOrientation = GameObject.Find("CenterEyeAnchor").transform.forward;
+		Ray playerRay = new Ray(transform.position, playerOrientation);
+		if (Physics.Raycast (playerRay, out hit, armLength)) {
+			Collider col = hit.collider;
+			if (col.tag == "key") {
+				handedObject = col.gameObject;
+				handedObject.SetActive(false);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if (takeItem ()) {
+				Debug.Log("Item catched !");
+			} else {
+				Debug.Log ("No item to catch...");
+			}
 		}
 	}
 }
