794d5464cf3ccc5b8ee06bb65be7737609e8d02c
megal000l <disturbman52@gmail.com>
2015-11-22 02:08:19 +0100

    spawner and stuff

--- Assets/Scripts/spawnerBehaviour.cs
+++ Assets/Scripts/spawnerBehaviour.cs
@@ -17,21 +17,45 @@ public class spawnerBehaviour : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		int n = Mathf.RoundToInt( Mathf.Sqrt ((nbItems + 1)/2 +1)) +1;
+		float deltaX = (maxX - minX) / n;
+		float deltaY = (maxY - minY) / n;
+		float deltaZ = (maxZ - minZ) / 2;
+		float radius = Mathf.Sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ) /2;
+
 		Random.seed = 13;
 		for (int i = 0; i<nbItems; i++) {
-			float v = Random.value * 100;
+			float v;
+			float posX ;
+			float posY ;
+			float posZ ;
+			bool isValid = false;
+			Vector3 posVecteur;
+
+			do{				
+				v = Random.value * 100;
+				int c = Mathf.RoundToInt(v) % (2*n*n) +1;
+				posX = minX + deltaX/2 + ((c-1)%n) * deltaX;
+				posY = minY + deltaY/2 + (c/n) * deltaY;
+				posZ = minZ + deltaZ/2 + (c%2) * deltaZ;
+				posVecteur = new Vector3(posX, posY, posZ);
+				isValid = Physics.CheckSphere(posVecteur, radius)
+					&& (posX>minX) && (posY>minX) && (posZ>minZ)
+					&& (posX<maxX) && (posY < maxY) && ( posZ<maxZ);
+				print (c);
+			}while (!isValid);
+			
+			print ("good");
+
 			if (v<=coef[0]){
 				GameObject o = (GameObject)PrefabUtility.InstantiatePrefab(spawns[0]);
-				print (v);
-				o.transform.position = new Vector3(0,0,0);			
+				o.transform.position = posVecteur;			
 			}else if (v<=coef[0]+coef[1]){
 				GameObject o = (GameObject)PrefabUtility.InstantiatePrefab(spawns[1]);
-				print (v);
-				o.transform.position = new Vector3(1,0,0);
+				o.transform.position = posVecteur;
 			}else{
 				GameObject o = (GameObject)PrefabUtility.InstantiatePrefab(spawns[2]);
-				print (v);		
-				o.transform.position = new Vector3(2,0,0);
+				o.transform.position = posVecteur;
 			}
 		}	
 	}
